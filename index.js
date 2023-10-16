8718b03c1a55b72f58584942a7d18a8291a9716d
Laureane Sousa <proflaureanesousa@gmail.com>
2023-10-16 19:07:22 -0300

    implementando poliformismo

--- index.js
+++ index.js
@@ -16,12 +16,30 @@ class Usuario extends Pessoa{
 }
 
 infos = function(){
-    return this.nome + " " + this.salario;
+    return this.nome + " " + this.email;
 }
 
 
+}
+
+
+class Comprador extends Pessoa{
+    constructor (nome, dn, genero,  end, cel, email, senha, compras){
+        super(nome, dn, genero, end, cel);
+        this.email = email;
+        this.senha = senha; 
+        this.compras = compras;
+}
+
+infos = function(){
+    return this.nome + " " + this.compras;
+}
 }
 
 const usuario1 = new Usuario("leonardo", "24/1/60","m", "rua matriz do Carmo n:80", 964781000, "aha@gmail.com", "hyt0915");
 
 console.log(usuario1);
+
+const usuario2 = new Usuario("maria", "4/11/2006","f", "rua do Carmo n:102", 964347810, "maria@gmail.com", "gusnb23");
+
+console.log(usuario2);
